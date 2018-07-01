7192c04ab9330e61bb5b8c51e45a5e54e36bc8fb
shimoniHadas <shimoni.hadas@gmail.com>
2018-07-01 21:09:18 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CenterComponent } from './center/center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    FooterComponent,
+    HeaderComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule
